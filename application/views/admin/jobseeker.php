49e2b6865f711bbd9d4ccc92764ee85bbe89e32e
mahisataruna <mahisataruna4@gmail.com>
2023-01-30 23:11:38 +0700

    Add jobseeker view page

--- application/views/admin/jobseeker.php
+++ application/views/admin/jobseeker.php
@@ -9,6 +9,58 @@
      </div>
 
      <!-- Start content -->
+     <div class="row">
+        <div class="col-lg-9 col-md col-sm mb-4">
+            <div class="card shadow mb-4">
+                                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary"><i class="fas fa-fw fa-solid fa-users fa-sm"></i> Data Employes</h6>
+                    <div class="dropdown no-arrow">
+                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                            <div class="dropdown-header">Dropdown Header:</div>
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+                    <div class="table-responsive table-sm" style="height: 320px;overflow: auto;">
+                        <div class="table-responsive">
+                            <table id="dataTable" class="table table-bordered table-hover table-xs" width="100%" cellspacing="0">
+                                <thead class="thead-light">
+									<tr>
+										<th class="sm" style="text-align: center;">#</th>
+										<th class="sm">Name</th>
+                                        <th class="sm">Company Name</th>
+                                        <th class="sm" style="text-align: center;">Status</th>
+										<th style="text-align: center;">Action</th>
+									</tr>
+								</thead>
+                                <tbody>
+                                    <tr>
+                                        <td width="2%" style="text-align: center;">1</td>
+                                        <td width="20%">Mahisa Taruna</td>
+                                        <td width="20%">PT. ABCD</td>
+                                        <td width="3%" style="text-align: center;">
+                                            <i class="fas fa-fw fa-solid fa-check fa-sm text-primary"></i>
+                                        </td>
+                                        <td width="4%" style="text-align: center;">
+											<a href="" onclick="return confirm('See detail account?')" class="btn btn-sm btn-info"><i class="fas fa-fw fa-solid fa-eye fa-sm"></i> Details</a>
+										</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
      <!-- End -->
 
--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -43,29 +43,6 @@
             <!-- Divider -->
             <hr class="sidebar-divider">
 
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item active">
-                <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
-                    aria-controls="collapsePages">
-                    <i class="fas fa-fw fa-folder"></i>
-                    <span>User Pages</span>
-                </a>
-                <div id="collapsePages" class="collapse show" aria-labelledby="headingPages"
-                    data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Users:</h6>
-                        <a class="collapse-item" href="#">Profile</a>
-                        <a class="collapse-item" href="#">Edit Profile</a>
-                        <a class="collapse-item" href="#">Change Password</a>
-                    </div>
-                </div>
-            </li>
-
-            
-
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
-
             <!-- Sidebar Toggler (Sidebar) -->
             <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
